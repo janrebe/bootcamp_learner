5fda4ab2621d29f489340dce97b8334cee81a0d3
janrebe <24f1002330@ds.study.iitm.ac.in>
2025-10-09 19:15:45 +0530

    starting a restaurant app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/', methods = ['GET'])
+def home():
+    return render_template('landingpage.html')
+@app.route('/search', methods=['GET'])
+def search():
+    return render_template('search.html')
+@app.route('/mybookings',methods=['GET'])
+def mybookings():
+    return render_template('mybookings.html')
+if __name__=="__main__":
+    app.debug=True
+    app.run(host='0.0.0.0', port=3000)
